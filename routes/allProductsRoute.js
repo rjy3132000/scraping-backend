fe352001332ec013c2db7c3cf6abb02d3c32fb39
Rajesh <ry292000@gmail.com>
2024-05-24 23:26:57 +0530

    added the get route

--- routes/allProductsRoute.js
+++ routes/allProductsRoute.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
-const {scrapeHubbardData} = require("../controllers/scrapeHubbardDataAfterLogin");
-const {scrapeHughesData} = require("../controllers/scrapeHughesDataAfterLogin");
-const {scrapeReeceData} = require("../controllers/scrapeReeceDataAfterLogin");
+const {
+  scrapeHubbardData,
+} = require("../controllers/scrapeHubbardDataAfterLogin");
+const {
+  scrapeHughesData,
+} = require("../controllers/scrapeHughesDataAfterLogin");
+const { scrapeReeceData } = require("../controllers/scrapeReeceDataAfterLogin");
 
-
+router.route("/").get((req, res) => {
+  res.send("API is working");
+});
 router.post("/scrape-Hubbard-data", scrapeHubbardData);
 router.post("/scrape-hughes-data", scrapeHughesData);
 router.post("/scrape-reece-data", scrapeReeceData);
-
 
 module.exports = router;
